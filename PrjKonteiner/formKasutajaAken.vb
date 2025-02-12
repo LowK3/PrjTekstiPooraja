0c8076777a30d25434053305f19acf39c0c60898
Rando Kübar <rakuba@taltech.ee>
2025-02-12 13:27:17 +0200

    Pööra funktsiooniga valmis

--- PrjKonteiner/formKasutajaAken.vb
+++ PrjKonteiner/formKasutajaAken.vb
@@ -1,9 +1,20 @@
-﻿Public Class formKasutajaAken
+﻿Imports System.Drawing.Text
+Imports System.Runtime.CompilerServices
+Imports PrjTekstiPoorajaKomponent
+
+Public Class formKasutajaAken
     Private Sub formKasutajaAken_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lblPooramineFunktsiooniga.Click
+    Private Sub btnPooraFunktsiooniga_Click(sender As Object, e As EventArgs) _
+        Handles btnPooraFunktsiooniga.Click
+        Dim pooraja As PrjTekstiPoorajaKomponent.ITeisendaja
 
+        pooraja = New PrjTekstiPoorajaKomponent.TekstiPooraja
+
+        pooraja.Tekst = txtSisendTekst.Text
+
+        txtValjundTekst1.Text = pooraja.PooraTekst
     End Sub
 End Class
