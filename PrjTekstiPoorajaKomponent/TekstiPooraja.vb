d5bc4ada701cc9c0cccea4a6e732bbf9db0933a9
Rando Kübar <rakuba@taltech.ee>
2025-02-12 12:38:42 +0200

    Lisatud klassi TekstiPooraja atribuudid

--- PrjTekstiPoorajaKomponent/TekstiPooraja.vb
+++ PrjTekstiPoorajaKomponent/TekstiPooraja.vb
@@ -1,3 +1,43 @@
 ﻿Public Class TekstiPooraja
+    Implements ITeisendaja
 
+    ' Klassi TekstiPooraja atribuudid
+    Private AlguseSymbol As Integer
+    Private LoppSymbol As Integer
+    Private PooratavTekst As String
+
+    Private Property EsimeseTaheAsciiKood As Integer Implements ITeisendaja.EsimeseTaheAsciiKood
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As Integer)
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Private Property ViimaseTaheAsciiKood As Integer Implements ITeisendaja.ViimaseTaheAsciiKood
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As Integer)
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Private Property Tekst As String Implements ITeisendaja.Tekst
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As String)
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Private Sub TeisendaTekst(ByRef sisendTekst As String) Implements ITeisendaja.TeisendaTekst
+        Throw New NotImplementedException()
+    End Sub
+
+    Private Function PooraTekst() As String Implements ITeisendaja.PooraTekst
+        Throw New NotImplementedException()
+    End Function
 End Class
