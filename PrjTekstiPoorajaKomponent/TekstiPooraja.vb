b908d4c13ebaaf8795751734635077d832aabe94
Rando Kübar <rakuba@taltech.ee>
2025-02-12 12:49:31 +0200

    Konfigureeritud Property elemendid TekstiPoorajas

--- PrjTekstiPoorajaKomponent/TekstiPooraja.vb
+++ PrjTekstiPoorajaKomponent/TekstiPooraja.vb
@@ -2,42 +2,44 @@
     Implements ITeisendaja
 
     ' Klassi TekstiPooraja atribuudid
-    Private AlguseSymbol As Integer
+    Private AlgusSymbol As Integer
     Private LoppSymbol As Integer
     Private PooratavTekst As String
 
     Private Property EsimeseTaheAsciiKood As Integer Implements ITeisendaja.EsimeseTaheAsciiKood
         Get
-            Throw New NotImplementedException()
+            Return AlgusSymbol
         End Get
         Set(value As Integer)
-            Throw New NotImplementedException()
+            AlgusSymbol = value
         End Set
     End Property
 
     Private Property ViimaseTaheAsciiKood As Integer Implements ITeisendaja.ViimaseTaheAsciiKood
         Get
-            Throw New NotImplementedException()
+            Return LoppSymbol
         End Get
         Set(value As Integer)
-            Throw New NotImplementedException()
+            LoppSymbol = value
         End Set
     End Property
 
     Private Property Tekst As String Implements ITeisendaja.Tekst
         Get
-            Throw New NotImplementedException()
+            Return PooratavTekst
         End Get
         Set(value As String)
-            Throw New NotImplementedException()
+            PooratavTekst = value
         End Set
     End Property
 
-    Private Sub TeisendaTekst(ByRef sisendTekst As String) Implements ITeisendaja.TeisendaTekst
-        Throw New NotImplementedException()
+    Private Sub TeisendaTekst(ByRef sisendTekst As String) _
+        Implements ITeisendaja.TeisendaTekst
+        PooratavTekst = StrReverse(sisendTekst)
     End Sub
 
-    Private Function PooraTekst() As String Implements ITeisendaja.PooraTekst
-        Throw New NotImplementedException()
+    Private Function PooraTekst() As String _
+        Implements ITeisendaja.PooraTekst
+        Return StrReverse(PooratavTekst)
     End Function
 End Class
